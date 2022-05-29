229ae9bdcc4955f609eaf05f067cd0b9e917f389
amani3g <amani.m.garvin@gmail.com>
2022-05-29 13:11:24 -0700

    Updating query

--- query_eaot.py
+++ query_eaot.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-eaot_base = "https://catalogs.mast.stsci.edu/api/v0.1/eaot/search"
-metadata = eaot_base + "/metadata.json"
-json_decorator = '.json'
+EAOT_BASE = "https://catalogs.mast.stsci.edu/api/v0.1/eaot/search"
+METADATA = EAOT_BASE + "/metadata.json"
+JSON_DECORATOR = '.json'
 
 COLUMNS = {
             "Planet_Name": "Planet name", 
@@ -12,7 +12,7 @@ COLUMNS = {
             "SNR_Emission_K_mag": "K-band Transmission SNR relative to HD 209458 b",
             "Rp": "Planet radius (Jupiter radii)",
             "Mp": "Planet mass (Jupiter masses)",
-            "Tday": "Dayside Temperature (Kelvin)"",
+            "Tday": "Dayside Temperature (Kelvin)",
             "Teq": "Planet equilibrium temperature (K)",
             "log10g_p": "Planet surface gravity",
             "Period": "Planet orbital period (d)",
@@ -24,6 +24,9 @@ COLUMNS = {
             "Transit_Flag": " Planet transit flag (0- no transit, 1- transit)",
             "Catalog_Name": "Planet source catalog name"
         }
+
+class EAOTException(Exception):
+    """Class for EAOT Exceptions"""
 
 def make_request(string):
     try:
@@ -41,22 +44,46 @@ def jtext(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-def jdict(text) --> dict:
+def jdict(text) -> dict:
     dict = json.loads(text)
     return dict
 
-def query_column(columns) --> dict:
+def query_columns(columns, sort=None, sort_on_col=None) -> dict:
     """ Query columns name, and return dictionary of column
-    columns - List of column names of name
+    columns (dict) - Dictionary of column names and bounds
+        e.g.
+        columns = {
+                    'col_name': {'upper': None, 'lower': foo}, 
+                    'col_name2' : {'upper': bar, 'lower': foo}
+                    }
+    sort (string) -  'asc' or 'desc'
+    sort_on_col (string) - column to sort on
     return:
         results - json dictionary of column values 
     """
-    query = eaot_base
-    for column in columns:
-        query += "?columns=" + column
-        if len(columns) > 1 and column != column[-1]:
-            query += "&"
-    query += json_decorator
+    query = EAOT_BASE + JSON_DECORATOR
+    for column in columns.keys():
+        if column in COLUMNS.keys():
+            query += "?columns=" + column
+            if columns[column]['upper']:
+                query += ".gte=" + str(column[upper])
+            if columns[column]['lower']:
+                query += ".lte=" + str(column[lower])
+            if len(columns) > 1 and column != column[-1]:
+                query += "&"
+        else:
+            raise(EAOTException("Invalid column {}".format(column)))
+    if sort:
+        if sort_on_col in COLUMNS.keys() and (sort == '.asc' or sort == 'desc'):
+            query += "sort_by=" + sort_on_col + "." + sort
+        else:
+            raise(EAOTException("Invalid sorting on column: {} sorting: {}".format(sort_on_col, sort)))
+    
+    print(query)
 
     return make_request(query)
 
+query_input = {"Planet_Name": {'upper': None, 'lower': None}}
+
+output_planet_name = query_columns(query_input)
+print(output_planet_name)
